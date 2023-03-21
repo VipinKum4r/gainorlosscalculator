ce1038d25897ce531379705547f6abbb20b00f10
Vipin Kumar <77539270+VipinKum4r@users.noreply.github.com>
2023-03-21 23:09:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        entry_price = float(request.form['entry'])
+        exit_price = float(request.form['exit'])
+        percent_change = ((exit_price - entry_price) / entry_price) * 100
+        if exit_price > entry_price:
+            result = f"{percent_change:.2f}% gain"
+        else:
+            result = f"{percent_change:.2f}% loss"
+        return render_template('result.html', result=result)
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
